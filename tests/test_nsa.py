9ecac7b0f1374ed4febc2f5d3a970b5d7ff5d3f6
Yu Zhang <yzhang.cs@outlook.com>
2025-02-21 10:53:13 -0800

    Add test cases for triton kernels

---
+++ tests/test_nsa.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+import torch
+import triton
+
+from fla.ops.common.utils import prepare_token_indices
+from native_sparse_attention.ops.naive import naive_nsa
+from native_sparse_attention.ops.parallel import parallel_nsa
+
+
+def get_abs_err(x, y):
+    return (x-y).flatten().abs().max().item()
+
+
+def get_err_ratio(x, y):
+    err = (x-y).flatten().square().mean().sqrt().item()
+    base = (x).flatten().square().mean().sqrt().item()
+    return err / base
+
+
+def assert_close(prefix, ref, tri, ratio):
+    msg = f"{prefix} diff: {get_abs_err(ref, tri):.6f} ratio: {get_err_ratio(ref, tri):.6f}"
+    print(msg)
+    assert get_err_ratio(ref, tri) < ratio, msg
+
+
+@pytest.mark.parametrize("B", [1])
+@pytest.mark.parametrize("T", [256, 1024, 2000])
+@pytest.mark.parametrize("H", [4])
+@pytest.mark.parametrize("HQ", [64])
+@pytest.mark.parametrize("D", [100, 64])
+@pytest.mark.parametrize("S", [16])
+@pytest.mark.parametrize("block_size", [32])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+@pytest.mark.parametrize("scale", [0.1])
+def test_parallel(
+    B: int,
+    H: int,
+    HQ: int,
+    T: int,
+    D: int,
+    S: int,
+    block_size: int,
+    dtype: torch.dtype,
+    scale: float
+):
+    torch.manual_seed(42)
+    os.environ['TRITON_F32_DEFAULT'] = 'ieee'
+
+    q = torch.randn((B, T, HQ, D), dtype=dtype, device='cuda').requires_grad_(True)
+    k = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
+    v = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
+    do = torch.randn((B, T, HQ, D), dtype=dtype, device='cuda')
+
+    indices = torch.full((B, T, H, S), T, dtype=torch.long, device='cuda')
+    for b in range(B):
+        for t in range(T):
+            for h in range(H):
+                i_i = torch.randperm(max(1, triton.cdiv(t, block_size)))[:S]
+                indices[b, t, h, :len(i_i)] = i_i
+    indices = indices.sort(-1)[0]
+
+    ref = naive_nsa(q=q, k=k, v=v, indices=indices, block_size=block_size, scale=scale)
+    ref.backward(do)
+    ref_dq, q.grad = q.grad.clone(), None
+    ref_dk, k.grad = k.grad.clone(), None
+    ref_dv, v.grad = v.grad.clone(), None
+
+    tri = parallel_nsa(q=q, k=k, v=v, indices=indices, block_size=block_size, scale=scale)
+    tri.backward(do)
+    tri_dq, q.grad = q.grad.clone(), None
+    tri_dk, k.grad = k.grad.clone(), None
+    tri_dv, v.grad = v.grad.clone(), None
+
+    assert_close(" o", ref, tri, 0.005)
+    assert_close("dq", ref_dq, tri_dq, 0.005)
+    assert_close("dk", ref_dk, tri_dk, 0.005)
+    assert_close("dv", ref_dv, tri_dv, 0.005)
+
+
+@pytest.mark.parametrize("N", [4])
+@pytest.mark.parametrize("T", [64, 128, 200, 250, 256, 300, 400, 512, 1000, 2048])
+@pytest.mark.parametrize("H", [4])
+@pytest.mark.parametrize("HQ", [64])
+@pytest.mark.parametrize("D", [100, 64])
+@pytest.mark.parametrize("S", [16])
+@pytest.mark.parametrize("block_size", [32])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+def test_parallel_varlen(
+    N: int,
+    T: int,
+    H: int,
+    HQ: int,
+    D: int,
+    S: int,
+    block_size: int,
+    dtype: torch.dtype,
+):
+    torch.manual_seed(42)
+    os.environ['TRITON_F32_DEFAULT'] = 'ieee'
+
+    # randomly split the sequence into N segments
+    offsets = torch.cat([
+        torch.tensor([0], dtype=torch.long),
+        torch.arange(16, T)[torch.randperm(T - 1)[:N-1]],
+        torch.tensor([T], dtype=torch.long)
+    ], 0).cuda().sort()[0]
+    # seq-first required for inputs with variable lengths
+    q = torch.randn((1, T, HQ, D), dtype=dtype, device='cuda').requires_grad_()
+    k = torch.randn((1, T, H, D), dtype=dtype, device='cuda').requires_grad_()
+    v = torch.randn((1, T, H, D), dtype=dtype, device='cuda').requires_grad_()
+    do = torch.randn((1, T, HQ, D), dtype=dtype, device='cuda')
+
+    indices = torch.full((1, T, H, S), T, dtype=torch.long, device='cuda')
+    seq_indices = prepare_token_indices(offsets).tolist()
+
+    for i in range(T):
+        _, t = seq_indices[i]
+        for h in range(H):
+            i_i = torch.randperm(max(1, triton.cdiv(t, block_size)))[:S]
+            indices[0, i, h, :len(i_i)] = i_i
+    indices = indices.sort(-1)[0]
+
+    ref = naive_nsa(
+        q=q,
+        k=k,
+        v=v,
+        indices=indices,
+        block_size=block_size,
+        cu_seqlens=offsets
+    )
+    ref.backward(do)
+    ref_dq, q.grad = q.grad.clone(), None
+    ref_dk, k.grad = k.grad.clone(), None
+    ref_dv, v.grad = v.grad.clone(), None
+
+    tri = parallel_nsa(
+        q=q,
+        k=k,
+        v=v,
+        indices=indices,
+        block_size=block_size,
+        cu_seqlens=offsets
+    )
+    tri.backward(do)
+    tri_dq, q.grad = q.grad.clone(), None
+    tri_dk, k.grad = k.grad.clone(), None
+    tri_dv, v.grad = v.grad.clone(), None
+
+    assert_close("  o", ref, tri, 0.004)
+    assert_close("dq", ref_dq, tri_dq, 0.005)
+    assert_close("dk", ref_dk, tri_dk, 0.005)
+    assert_close("dv", ref_dv, tri_dv, 0.005)
