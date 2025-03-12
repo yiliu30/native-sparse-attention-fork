3e8608c4d34c562395d3c844847a963da3ea0a39
Hanyuezhuohua <1697684739@qq.com>
2025-03-12 14:47:08 -0700

    update

--- native_sparse_attention/__init__.py
+++ native_sparse_attention/__init__.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from .ops.naive import naive_nsa, naive_nsa_with_compression
+from .ops.parallel import parallel_nsa, parallel_nsa_with_compression
+
+__all__ = [
+    'naive_nsa',
+    'parallel_nsa',
+    'naive_nsa_with_compression',
+    'parallel_nsa_with_compression'
+]
+
+
 __version__ = '0.0'
